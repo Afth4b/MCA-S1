307dc9976381e4d4abbd5ec900471e53ffb83c0b
Afth4b <aafthabismail1234@gmail.com>
2025-08-23 11:18:49 +0530

    merged LL

--- ds/linked-list.c
+++ ds/linked-list.c
@@ -15,10 +15,7 @@ struct Node* head = NULL;
 // Node creation
 struct Node* CreateNode(int data) {
     struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
+
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
