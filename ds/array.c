0c576e3b16861837bedf2602aa8e0d31f4d1d153
Afth4b <aafthabismail1234@gmail.com>
2025-09-12 10:55:22 +0530

    LAST DS

---
+++ ds/array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,a[];
+    printf("Enter the number of elements of array");
+    scanf("%d",&n);
+    printf("Enter the elements of array");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+}
